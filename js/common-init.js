03ffc69da9dfd30de49fd180f2d99e50a8bff95a
Hunter Chen <hunterflag@gmail.com>
2023-07-19 14:01:15 +0800

    just update

---
+++ js/common-init.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+ commit_init();
+ 
+ function commit_init(){
+	console.log("commit-init(): " + Date());
+/*
+	let element = document.getElementById("about-subway-studio");
+	element.title="<h1>1981</h1>";
+*/
+ }	 
---
+++ js/index-init.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+ index_init();
+ 
+ function index_init(){
+	console.log("index-init(): " + Date());
+
+	let element = document.getElementById("about-subway-studio");
+	element.title="<h1>1981</h1>";
+ }	 
