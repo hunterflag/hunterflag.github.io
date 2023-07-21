55c01337bd4a723f9db8b3f574aa39f871f2b3ee
Hunter Chen <hunterflag@gmail.com>
2023-07-21 18:33:17 +0800

    update

--- js/common-init.js
+++ js/common-init.js
@@ -1,16 +1,14 @@
 /**
  * 
  */
- commit_init();
  
- function commit_init(){
-	console.log("commit-init(): " + Date());
-/*
-	let element = document.getElementById("about-subway-studio");
-	element.title="<h1>1981</h1>";
-*/
-	
-	let date = document.getElementById("label-date");
-	date.innerHTML="<span>"+ Date() + "</span>";
- }	 
+common_init();
+ 
+function common_init(){
+	let date = Date();
+	console.log("commit-init(): " + date);
+
+	let content_date = document.getElementById("content-date");
+	content_date.innerHTML="<span>本頁載入時間: "+ date + "</span>";
+}	 
  
--- js/index-init.js
+++ js/index-init.js
@@ -1,11 +1,10 @@
 /**
  * 
  */
- index_init();
- 
- function index_init(){
-	console.log("index-init(): " + Date());
 
-	let element = document.getElementById("about-subway-studio");
-	element.title="<h1>1981</h1>";
- }	 
+index_init();
+ 
+function index_init(){
+	let date = Date();
+	console.log("index-init(): " + date);
+}	 
